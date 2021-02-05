01aac7b2b2bd3651dea110fff2850cc28a044166
silkehof <silke@silkehofmann.eu>
2021-02-05 20:22:50 +0100

    Add solution for exercise 4_5

--- part4/bloglist_backend/tests/total-likes.test.js
+++ part4/bloglist_backend/tests/total-likes.test.js
@@ -81,3 +81,92 @@ describe('total likes', () => {
     })
 
 })
+
+describe('favorite blog', () => {
+    const listWithNoBlog = []
+    const emptyObject = {}
+
+    test('of a list with no blogs returns {}', () => {
+        const result = listHelper.favoriteBlog(listWithNoBlog)
+        expect(result).toEqual(emptyObject)
+    })
+
+    const listWithOneBlog = [
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        }
+    ]
+
+    const topBlog =
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12,
+    }
+
+    test('of a list with one blogs returns its title, author and likes', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual(topBlog)
+    })
+
+    const listWithManyFavoriteBlogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 12,
+            __v: 0
+        }
+    ]
+    
+    test('of a list with multiple blogs returns title, author and likes of the blog with most likes', () => {
+        const result = listHelper.favoriteBlog(listWithManyFavoriteBlogs)
+        expect(result).toEqual(topBlog)
+    })
+})
--- part4/bloglist_backend/utils/list_helper.js
+++ part4/bloglist_backend/utils/list_helper.js
@@ -10,7 +10,25 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const blogLikes = blogs.map(blog => blog.likes)
+    const topLikes = Math.max(...blogLikes)
+
+    for (var i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes === topLikes) {
+            const blog = {
+                title: blogs[i].title,
+                author: blogs[i].author,
+                likes: blogs[i].likes
+            }
+            return blog
+        }
+    }
+    return {}
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes, 
+    favoriteBlog
 }
