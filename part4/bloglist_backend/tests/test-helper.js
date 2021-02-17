ccf17e809a4feb4ad6d1d8888b9ebf2c1b853770
silkehof <silke@silkehofmann.eu>
2021-02-17 21:47:05 +0100

    Add solution for exercise 4_8

---
+++ part4/bloglist_backend/tests/test-helper.js
@@ -0,0 +1,26 @@
+const Note = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+    },
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 13
+    },
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+    }
+]
+
+module.exports = {
+    initialBlogs
+}
