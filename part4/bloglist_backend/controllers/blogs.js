a9d50dfe82f9b5e90b64d259ec4709342695f3b2
silkehof <silke@silkehofmann.eu>
2021-03-05 00:50:34 +0100

    Add solution for exercise 4_21

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -4,52 +4,60 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog
-        .find({}).populate('user', { username: 1, name: 1 })
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
 
-    response.json(blogs)
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const body = request.body
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!request.token || !decodedToken.id) {
-        return response.status(401).json({ error: 'token missing or invalid' })
-    }
-    const user = await User.findById(decodedToken.id)
+  const body = request.body
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author, 
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-    })
-    const savedBlog = await blog.save()
+  const blog = new Blog({
+    title: body.title,
+    author: body.author, 
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+  const savedBlog = await blog.save()
     
-    if (blog.url === undefined || blog.title === undefined) {
-        response.status(400).json({ error: 'url or title missing'})
-    } else {
-        user.blogs = user.blogs.concat(savedBlog._id)
-        await user.save()
-        response.status(201).json(savedBlog)
-    }
+  if (blog.url === undefined || blog.title === undefined) {
+    response.status(400).json({ error: 'url or title missing'})
+  } else {
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
+  }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+  const blog = await Blog.findById(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  //console.log(blog.user)
+
+  if (blog.user.toString() !== decodedToken.id.toString()) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  } 
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    const blog = {
-        likes: body.likes,
-    }
+  const blog = {
+    likes: body.likes,
+  }
 
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    response.json(updatedBlog)
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- part4/bloglist_backend/controllers/users.js
+++ part4/bloglist_backend/controllers/users.js
@@ -3,32 +3,32 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User
-        .find({}).populate('blogs', { title: 1, author: 1, url: 1 })
+  const users = await User
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1 })
 
-    response.json(users)
+  response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
-    const body = request.body
-    if (body.password === undefined || body.password.length < 3) {
-        console.log('poop')
-        response.status(400).json({ error: 'invalid password' })
-        return
-    } 
+  const body = request.body
+  if (body.password === undefined || body.password.length < 3) {
+    console.log('poop')
+    response.status(400).json({ error: 'invalid password' })
+    return
+  } 
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-    const user = new User({
-        username: body.username,
-        name: body.name,
-        passwordHash,
-    })
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
 
-    const savedUser = await user.save()
+  const savedUser = await user.save()
 
-    response.json(savedUser)
+  response.json(savedUser)
 })
 
 module.exports = usersRouter
