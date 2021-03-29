2fbfff8d2f104076e53e357bb491d6a24a8c1898
silkehof <silke@silkehofmann.eu>
2021-03-30 00:49:00 +0200

    Add solution for exercise 5_14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -23,3 +23,22 @@ test('renders content - author, title and blog creator', () => {
   expect(component.getByText(blog.likes.toString())).not.toBeVisible()
 })
 
+test('renders url and likes when view button is clicked', () => {
+  const blog = {
+    author: 'Mr Knowitall',
+    title: 'Blog',
+    url: 'www.blog.de',
+    likes: 10,
+    user: '60468e0c254d186a90edbee1'
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+
+  expect(component.getByText(blog.url)).toBeVisible()
+  expect(component.getByText(blog.likes.toString())).toBeVisible()
+})
