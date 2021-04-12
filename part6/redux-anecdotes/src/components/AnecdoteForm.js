9631d08d635dcf1f2e1a52339844448450d7faec
silkehof <silke@silkehofmann.eu>
2021-04-13 00:03:08 +0200

    Format redux_anecdote project

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -4,24 +4,24 @@ import { createAnecdote } from '../reducers/anecdoteReducer'
 
 
 const AnecdoteForm = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
-        event.preventDefault()
-        const content = event.target.anecdote.value
-        event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
-    }
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
 
-    return (
-      <div>
-        <h2>create new</h2>
-        <form onSubmit={addAnecdote}>
-          <div><input name="anecdote"/></div>
-          <button type="submit">create</button>
-        </form>
-      </div >
-    )
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
+    </div >
+  )
 }
 
 export default AnecdoteForm
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,19 +23,19 @@ const reducer = (state = initialState, action) => {
   //console.log('state now: ', state)
   //console.log('action', action)
   switch(action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.data]
-    case 'VOTE':
-      const id = action.data.id
-      console.log(id)
-      const anecdoteToChange = state.find(a => a.id === id)
-      const changedAnecdote = {
-        ...anecdoteToChange,
-        votes: anecdoteToChange.votes + 1
-      }
-      return state.map(anecdote =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      )
+  case 'NEW_ANECDOTE':
+    return [...state, action.data]
+  case 'VOTE':
+    const id = action.data.id
+    console.log(id)
+    const anecdoteToChange = state.find(a => a.id === id)
+    const changedAnecdote = {
+      ...anecdoteToChange,
+      votes: anecdoteToChange.votes + 1
+    }
+    return state.map(anecdote =>
+      anecdote.id !== id ? anecdote : changedAnecdote
+    )
   default:
     return state
   }
